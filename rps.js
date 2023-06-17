3936e002967cabca0d6a541a096da835af0ee806
ALangon33 <alangon03@gmail.com>
2023-06-17 15:18:27 -0700

    Turn random # into a choice

--- rps.js
+++ rps.js
@@ -2,8 +2,18 @@
 // Select @ random opponent choice
 function getComputerChoice() {
     // Get Random # between 0 - 2
-    Math.floor(Math.random() * 3);
+    let computerDecision = Math.floor(Math.random() * 3);
+
+
     // If 0 : Rock, if 1 : Paper, else Scissors
+    if (computerDecision === 0) {
+        computerThrow = "Rock";
+    } else if (computerDecision === 1) {
+        computerThrow = "Paper";
+    } else {
+        computerThrow = "Scissors";
+    };
+
 };
 // Compare user to opponent
 // Reveal outcome to user
