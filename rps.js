ccbf889ab1bec5f6dc4611ef6250ca19365dbd6e
ALangon33 <alangon03@gmail.com>
2023-06-17 15:10:28 -0700

    Define getComputerChoice and pseudocode it

--- rps.js
+++ rps.js
@@ -1,4 +1,9 @@
 // Request user input (Rock, Paper, or Scissors)
 // Select @ random opponent choice
+function getComputerChoice() {
+    // Get Random # between 0 - 2
+    Math.floor(Math.random() * 3);
+    // If 0 : Rock, if 1 : Paper, else Scissors
+};
 // Compare user to opponent
 // Reveal outcome to user
