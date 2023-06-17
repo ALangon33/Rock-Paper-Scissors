b88a12d237d644ed0fc602541f07b44aac77544b
ALangon33 <alangon03@gmail.com>
2023-06-17 15:22:05 -0700

    Get user input and define play function

--- rps.js
+++ rps.js
@@ -1,9 +1,10 @@
 // Request user input (Rock, Paper, or Scissors)
+let playerChoice = prompt("Rock, Paper, or Scissors?");
 // Select @ random opponent choice
 function getComputerChoice() {
     // Get Random # between 0 - 2
     let computerDecision = Math.floor(Math.random() * 3);
-
+    console.log(computerDecision);
 
     // If 0 : Rock, if 1 : Paper, else Scissors
     if (computerDecision === 0) {
@@ -13,7 +14,14 @@ function getComputerChoice() {
     } else {
         computerThrow = "Scissors";
     };
+    console.log(computerThrow);
 
 };
+
+getComputerChoice();
 // Compare user to opponent
+function playRPS() {
+
+};
+
 // Reveal outcome to user
