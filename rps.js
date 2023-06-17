8170b611726419ed318e5c8105b20f1e3a1f13a6
ALangon33 <alangon03@gmail.com>
2023-06-17 15:48:17 -0700

    Fix some scope issues

--- rps.js
+++ rps.js
@@ -1,12 +1,7 @@
-// Request user input (Rock, Paper, or Scissors)
-function getPlayerChoice() {
-    let playerThrow = prompt("Rock, Paper, or Scissors?");
-};
-
 // Select @ random opponent choice
 function getComputerChoice() {
     // Get Random # between 0 - 2
-    let computerDecision = Math.floor(Math.random() * 3);
+    computerDecision = Math.floor(Math.random() * 3);
     console.log(computerDecision);
 
     // If 0 : Rock, if 1 : Paper, else Scissors
@@ -23,16 +18,22 @@ function getComputerChoice() {
 
 // Compare user to opponent
 function playRPS() {
-    getPlayerChoice();
+// Request user input (Rock, Paper, or Scissors)
+    playerThrow = prompt("Rock, Paper, or Scissors?");
     console.log(playerThrow);
     getComputerChoice();
     console.log(computerThrow);
 
-    if (playerThrow == 'rock' && computerThrow == 'paper') || (playerThrow == 'paper' && computerThrow == 'scissors') || (playerThrow == 'scissors' && computerThrow == 'rock') {
+    if ((playerThrow == 'rock' && computerThrow == 'paper') || (playerThrow == 'paper' && computerThrow == 'scissors') || (playerThrow == 'scissors' && computerThrow == 'rock')) {
         // If lost display defeat message
+        console.log("Get Rekt");
         // If won display victory message
+    } else {
+        console.log("Victory");
     }
 
 };
+
+playRPS();
 
 // Reveal outcome to user
